c4d839f97f7c618407eb51d653138f8c8e5e7229
Devya <Devya@Devya-HP>
2015-03-18 22:55:05 -0400

    Lab 3 calculator

--- lab3/src/lab3/Calculator.java
+++ lab3/src/lab3/Calculator.java
@@ -1,6 +1,5 @@
 package lab3;
 
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
@@ -10,42 +9,63 @@ import java.util.Scanner;
 public class Calculator {
 
     public static void main(String[] args) {
-//Assign string variable
-        String str1 = "/", str2 = "*", str3 = "+", str4 = "-";
-//formatting numbers upto 2 decimal places
-        DecimalFormat df = new DecimalFormat("#.00");
-//Create a Scanner
-        Scanner keyboard = new Scanner(System.in);
-//Prompt the user to enter operator
-        System.out.println("Please enter an operator: ");
-        String operator = keyboard.nextLine();
-//Prompt the user to enter number
-        System.out.println("Please enter a number: ");
-        double num1 = keyboard.nextDouble();
-//Prompt the user to enter another number
-        System.out.println("Please enter another number: ");
-        double num2 = keyboard.nextDouble();
-        if (num2 != 0) {
-            if (operator.equals(str1)) {
-                System.out.println(num1 + "/" + num2 + " = " + df.format(num1 / num2));
-            } else {
-                if (operator.equals(str2)) {
-                    System.out.println(num1 + "*" + num2 + " = " + df.format(num1 * num2));
-                } else {
-                    if (operator.equals(str3)) {
-                        System.out.println(num1 + "+" + num2 + " = " + df.format(num1 + num2));
-                    } else {
-                        if (operator.equals(str4)) {
-                            System.out.println(num1 + "-" + num2 + " = " + df.format(num1 - num2));
-                        } else {
-                            System.out.println(operator + " is not a valid operator");
+        //Assign string variable
+        String str1 = "+", str2 = "-", str3 = "*", str4 = "/";
 
-                        }
-                    }
+        //Create a Scanner
+        Scanner keyboard = new Scanner(System.in);
+
+        //Prompt the user to enter operator
+        System.out.println("Enter an operator: ");
+        String operator = keyboard.nextLine();
+
+        //Prompt the user to enter number
+        System.out.println("Enter an integer: ");
+        int num1 = keyboard.nextInt();
+
+        //Prompt the user to enter another number
+        System.out.println("Enter another integer: ");
+        int num2 = keyboard.nextInt();
+
+        if (operator.equals(str1)) {
+
+            System.out.println(num1 + "+" + num2 + " = " + (num1 + num2));
+
+        } else if (operator.equals(str2)) {
+
+            System.out.println(num1 + "-" + num2 + " = " + (num1 - num2));
+
+        } else if (operator.equals(str3)) {
+
+            System.out.println(num1 + "*" + num2 + " = " + (num1 * num2));
+
+        } else if (operator.equals(str4)) {
+
+            if (num2 != 0) {
+
+                int reminder = num1 % num2;
+
+                if ((reminder == 0)) {
+
+                    System.out.println(num1 + "/" + num2 + " = " + (num1 / num2));
+
+                } else {
+
+                    System.out.println(num1 + "/" + num2 + " = " + (num1 / num2) + " and " + reminder + "/" + num2);
+
                 }
+
+            } else {
+
+                System.out.println("Divide by zero error !");
+
             }
+
         } else {
-            System.out.println("Divide by zero error !");
+
+            System.out.println(operator + " is not a valid operator");
+
         }
+
     }
 }
